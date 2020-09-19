090dbfafafb320ce61a8dbb340be0203addb3be2
Uma Shankar <56968738+Umashankar-live@users.noreply.github.com>
2020-09-19 16:20:51 +0530

    small changes

--- Backend/Test-microservice/src/main/java/com/cg/controller/TestController.java
+++ Backend/Test-microservice/src/main/java/com/cg/controller/TestController.java
@@ -1,20 +1,17 @@
 package com.cg.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
 import com.cg.bean.Tests;
 import com.cg.service.TestServiceInterface;
 
