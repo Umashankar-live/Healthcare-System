120bf0b08575bc35433112b0e706921166f3a074
Shekhar Kankale <shekharkankale@gmail.com>
2020-10-30 01:22:53 +0530

    Little Changes in Diagnostic Center
    Simple Swagger Added
    And Some Exceptions Are Added

--- Backend/DiagnosticCenter-microservice/src/main/java/com/cg/DiagnosticCenterMicroserviceApplication.java
+++ Backend/DiagnosticCenter-microservice/src/main/java/com/cg/DiagnosticCenterMicroserviceApplication.java
@@ -1,11 +1,22 @@
 package com.cg;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class DiagnosticCenterMicroserviceApplication {
 
 	@Bean
@@ -15,6 +26,20 @@ public class DiagnosticCenterMicroserviceApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DiagnosticCenterMicroserviceApplication.class, args);
+	}
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.cg")).build().apiInfo(myApiInfo());
+	}
+
+	private ApiInfo myApiInfo() {
+		// for version 2.9.1
+		ApiInfo apiInfo = new ApiInfo("SPRING WITH SWAGGER API", "API CREATION", "1.0", "Free to Use",
+				new Contact("Shekhar", "/", "shekharkankale@gmail.com"), "API licence", "/",
+				Collections.emptyList());
+		return apiInfo;
 	}
 
 }
--- Backend/DiagnosticCenter-microservice/src/main/java/com/cg/service/DiagnosticCenterServiceImpl.java
+++ Backend/DiagnosticCenter-microservice/src/main/java/com/cg/service/DiagnosticCenterServiceImpl.java
@@ -6,38 +6,70 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.cg.bean.DiagnosticCenters;
 import com.cg.dao.DiagnosticCenterDao;
+import com.cg.exception.NoValueFoundException;
+import com.cg.exception.NotPossibleException;
 
 @Service
 public class DiagnosticCenterServiceImpl implements DiagnosticCenterService {
 
 	@Autowired
 	private DiagnosticCenterDao dao;
-
+	
+	/***
+	 * This Function is used to add center
+	 */
 	@Override
 	public DiagnosticCenters addCenter(DiagnosticCenters center) {
-		return this.dao.save(center);
+		 if(this.dao.save(center)==null)
+			 throw new NotPossibleException("Cannot add this Center");
+		 
+		 return this.dao.save(center);
 	}
 
+	/***
+	 * THis function is used to delete center 
+	 */
 	@Override
 	public void deleteCenter(Integer centerId) {
 		this.dao.deleteById(centerId);
 	}
 
+	/***
+	 * This Function is used to search the center
+	 */
 	@Override
 	public DiagnosticCenters searchCenter(Integer centerId) {
-		return this.dao.findAll().stream().filter(x -> centerId.equals(x.getCenterId())).findAny().orElse(null);
+		if(this.dao.findAll().stream().filter(x -> centerId.equals(x.getCenterId())).findAny().orElse(null)==null) {
+			throw new NotPossibleException("Center not Found with this ID");
+		}
+		return this.dao.findAll().stream().filter(x -> centerId.equals(x.getCenterId())).findAny().orElse(null);		
+		
 	}
 
+	/***
+	 * This function is used to get all Centers 
+	 */
 	@Override
 	public List<DiagnosticCenters> getAllCenter() {
-		return this.dao.findAll();
+		 if(this.dao.findAll()==null)
+			 throw new NoValueFoundException("There is no Center in Table");
+		 
+		 return this.dao.findAll();
 	}
 
+	/***
+	 * This Function is used to update the center
+	 */
 	@Override
 	public DiagnosticCenters updateCenter(DiagnosticCenters center) {
+		if(this.dao.save(center)==null)
+			throw new NotPossibleException("Cannot upadte this center");
 		return this.dao.save(center);
 	}
 
+	/***
+	 * This function is used to count no of centers in the database
+	 */
 	@Override
 	public long countCenters() {
 		return dao.count();
